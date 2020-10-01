cc486d5bd9df41df9d854a8d89b91b0cf0cec861
Thiago E. Kalid DESKTOP <thiagokalid@protonmail.com>
2020-10-01 13:22:59 -0300

    Initial commit.

---
+++ Debug/FreeRTOS/Source/portable/GCC/ARM_CM3/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../FreeRTOS/Source/portable/GCC/ARM_CM3/port.c 
+
+OBJS += \
+./FreeRTOS/Source/portable/GCC/ARM_CM3/port.o 
+
+C_DEPS += \
+./FreeRTOS/Source/portable/GCC/ARM_CM3/port.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+FreeRTOS/Source/portable/GCC/ARM_CM3/%.o: ../FreeRTOS/Source/portable/GCC/ARM_CM3/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g3 -DSTM32F407xx -DUSE_HAL_DRIVER -I"D:\Desktop\tasks_FreeRTOS_qemu-master" -I"D:\Desktop\tasks_FreeRTOS_qemu-master\Libraries\CMSIS\Core\Include" -I"D:\Desktop\tasks_FreeRTOS_qemu-master\Libraries\CMSIS\Device\ST\STM32F4xx\Include" -I"D:\Desktop\tasks_FreeRTOS_qemu-master\Libraries\STM32F4xx_HAL_Driver\Inc" -I"D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include" -I"D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\portable\GCC\ARM_CM3" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Debug/FreeRTOS/Source/tasks.d
@@ -0,0 +1,40 @@
+FreeRTOS/Source/tasks.o: ../FreeRTOS/Source/tasks.c \
+ D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/FreeRTOS.h \
+ D:\Desktop\tasks_FreeRTOS_qemu-master/FreeRTOSConfig.h \
+ D:\Desktop\tasks_FreeRTOS_qemu-master\Libraries\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h \
+ D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/projdefs.h \
+ D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/portable.h \
+ D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/deprecated_definitions.h \
+ D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\portable\GCC\ARM_CM3/portmacro.h \
+ D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/mpu_wrappers.h \
+ D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/task.h \
+ D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/list.h \
+ D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/timers.h \
+ D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/task.h \
+ D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/stack_macros.h
+
+D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/FreeRTOS.h:
+
+D:\Desktop\tasks_FreeRTOS_qemu-master/FreeRTOSConfig.h:
+
+D:\Desktop\tasks_FreeRTOS_qemu-master\Libraries\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h:
+
+D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/projdefs.h:
+
+D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/portable.h:
+
+D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/deprecated_definitions.h:
+
+D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\portable\GCC\ARM_CM3/portmacro.h:
+
+D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/mpu_wrappers.h:
+
+D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/task.h:
+
+D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/list.h:
+
+D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/timers.h:
+
+D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/task.h:
+
+D:\Desktop\tasks_FreeRTOS_qemu-master\FreeRTOS\Source\include/stack_macros.h:

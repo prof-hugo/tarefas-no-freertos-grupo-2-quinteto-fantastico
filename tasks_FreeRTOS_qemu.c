8fcc75ba8287310ec8b361a96262ddd0e0411b02
Thiago E. Kalid DESKTOP <thiagokalid@protonmail.com>
2020-10-02 19:53:32 -0300

    Removendo algumas funções de exemplos anteriores.

--- tasks_FreeRTOS_qemu.c
+++ tasks_FreeRTOS_qemu.c
@@ -19,71 +19,6 @@
     }xBlinkParam;
 
 
-
-void vBlinkLED3(void *pvParam){
-	   int* param = pvParam;
-	  const int LED_index = LED3;
-	  const int time_delay = 530;
-	  BSP_LED_Init(LED_index);
-	  TickType_t xLastWakeTime;
-	  const TickType_t xPeriod = time_delay / portTICK_PERIOD_MS;
-	  xLastWakeTime = xTaskGetTickCount();
-
-	  while(1){
-	    BSP_LED_Toggle(LED_index);
-	    vTaskDelayUntil(&xLastWakeTime, xPeriod);
-	    taskYIELD();
-	  } // while
-} // vBlinkLED3
-
-void vBlinkLED4(void *pvParam){
-	   int* param = pvParam;
-	  const int LED_index = LED4;
-	  const int time_delay = 430;
-	  BSP_LED_Init(LED_index);
-	  TickType_t xLastWakeTime;
-	  const TickType_t xPeriod = time_delay / portTICK_PERIOD_MS;
-	  xLastWakeTime = xTaskGetTickCount();
-
-	  while(1){
-	    BSP_LED_Toggle(LED_index);
-	    vTaskDelayUntil(&xLastWakeTime, xPeriod);
-	    taskYIELD();
-	  } // while
-} // vBlinkLED4
-
-void vBlinkLED5(void *pvParam){
-	   int* param = pvParam;
-	  const int LED_index = LED5;
-	  const int time_delay = 330;
-	  BSP_LED_Init(LED_index);
-	  TickType_t xLastWakeTime;
-	  const TickType_t xPeriod = time_delay / portTICK_PERIOD_MS;
-	  xLastWakeTime = xTaskGetTickCount();
-
-	  while(1){
-	    BSP_LED_Toggle(LED_index);
-	    vTaskDelayUntil(&xLastWakeTime, xPeriod);
-	    taskYIELD();
-	  } // while
-} // vBlinkLED5
-
-void vBlinkLED6(void *pvParam){
-	  const int LED_index = LED6;
-	  const int time_delay = 230;
-	  BSP_LED_Init(LED_index);
-	  TickType_t xLastWakeTime;
-	  const TickType_t xPeriod = time_delay / portTICK_PERIOD_MS;
-	  xLastWakeTime = xTaskGetTickCount();
-
-	  while(1){
-	    BSP_LED_Toggle(LED_index);
-	    vTaskDelayUntil(&xLastWakeTime, xPeriod);
-	    taskYIELD();
-	  } // while
-} // vBlinkLED3
-
-
 void vBlinkLED(void *pvParam){
 	xBlinkParam* param = (xBlinkParam *) pvParam;
 	const int LED_index = param->LED_index;
@@ -100,10 +35,8 @@ void vBlinkLED(void *pvParam){
 	}
 }
 
-int param[] = {LED3, 500};
 void main(void){
   __HAL_RCC_GPIOD_CLK_ENABLE();
-
 
   static xBlinkParam LED_3 = {LED3 , 530};
   static xBlinkParam LED_4 = {LED4 , 430};
@@ -111,12 +44,6 @@ void main(void){
   static xBlinkParam LED_6 = {LED6 , 230};
 
 
-  /*
-  xTaskCreate(vBlinkLED3, "vBlinkLED3", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
-  xTaskCreate(vBlinkLED4, "vBlinkLED4", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
-  xTaskCreate(vBlinkLED5, "vBlinkLED5", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
-  xTaskCreate(vBlinkLED6, "vBlinkLED6", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
-*/
 
   xTaskCreate(vBlinkLED , "vBlinkLED3" , configMINIMAL_STACK_SIZE,  (void *) &LED_3  , tskIDLE_PRIORITY, NULL);
   xTaskCreate(vBlinkLED , "vBlinkLED4" , configMINIMAL_STACK_SIZE,  (void *) &LED_4  , tskIDLE_PRIORITY, NULL);
